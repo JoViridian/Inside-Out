cc44c08430677d6f6ac9df978a5972332adcef2c
JoViridian <joasvandriel@gmail.com>
2026-05-27 12:19:02 +0200

    Addded Lens View
    Added an image to imitate looking through a lens

--- Inside Out/Assets/Scripts/Zoom.cs
+++ Inside Out/Assets/Scripts/Zoom.cs
@@ -1,9 +1,16 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Zoom : MonoBehaviour
 {
     public Camera cam;
+    public GameObject lens;
+    private float lensScale;
 
+    private void Start()
+    {
+        //lensScale = 1.25f;
+    }
 
     // Update is called once per frame
     void Update()
@@ -11,11 +18,16 @@ public class Zoom : MonoBehaviour
         if(Input.GetKey(KeyCode.Mouse0))
         {
             cam.fieldOfView--;
+            //lensScale += 0.00833f; 
         }
         else
         {
             cam.fieldOfView++;
+            //lensScale -= 0.00833f;
         }
+
+        //Mathf.Clamp(lensScale, 1.25f, 1.5f);
         cam.fieldOfView = Mathf.Clamp(cam.fieldOfView, 30, 60);
+        //lens.transform.localScale = new Vector3(lensScale, lensScale, lensScale);
     }
 }
